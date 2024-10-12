f0832998c5be4b001644456ba0fdf8a40bb8bda1
M40 <modev.404@gmail.com>
2024-10-12 08:00:23 +0300

    Stucture & Layout: Column

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
-import 'package:basic/pages/homepage.dart';
-import 'package:basic/pages/screen0.dart';
-import 'package:basic/pages/screen1.dart';
+// import 'package:basic/pages/homepage.dart';
+// import 'package:basic/pages/screen0.dart';
+// import 'package:basic/pages/screen1.dart';
+import 'package:basic/structure_layout.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: "/",
-    routes: {
-      '/': (context) => const HomePage(),
-      '/screen0': (context) => const Screen0(),
-      '/screen1': (context) => const Screen1(),
-    },
-  ));
+  runApp(const StructureLayout());
 }
+
+// MaterialApp(
+// initialRoute: "/",
+// routes: {
+// '/': (context) => const HomePage(),
+// '/screen0': (context) => const Screen0(),
+// '/screen1': (context) => const Screen1(),
+// },
+// )
